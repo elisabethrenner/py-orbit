ed656e236ba1b89018b87b543b432e83e4611aec
Piotr <piotr.skowronski@cern.ch>
2019-06-12 17:44:56 +0200

    Added automatic detection of Intel compiler and setup of environment variables

--- setupEnvironment.sh
+++ setupEnvironment.sh
@@ -31,6 +31,36 @@ echo "Found Python include directory: $PYTHON_ROOT_INC"
 export PYTHONPATH=${PYTHONPATH}:${ORBIT_ROOT}/py:${ORBIT_ROOT}/lib
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${ORBIT_ROOT}/lib
 
+######################################
+
+
+IFPATH=`which ifort`
+if [ $? == 0 ]; then
+  IFDIR=${IFPATH///ifort}
+  IN_INTEL_TARGET_ARCH=${IFDIR#*bin/}
+  IFDIR=${IFDIR%/*} #must be twice
+  IFDIR=${IFDIR%/*}
+  IN_INTEL_TARGET_PLATFORM=${IFDIR##*/}
+  IFDIR=${IFDIR%/*}
+  echo $IFDIR $IN_INTEL_TARGET_ARCH $IN_INTEL_TARGET_PLATFORM
+else
+  echo "ifort is not configured"
+fi
+
+
+if [ -f  ${IFDIR}/${IN_INTEL_TARGET_PLATFORM}/bin/compilervars.sh ]; then
+  source ${IFDIR}/${IN_INTEL_TARGET_PLATFORM}/bin/compilervars.sh -arch $IN_INTEL_TARGET_ARCH -platform $IN_INTEL_TARGET_PLATFORM
+  export PROD_DIR INTEL_TARGET_ARCH
+  echo "Configured intel from ${IFDIR}/${IN_INTEL_TARGET_PLATFORM}"
+  echo "Exported  PROD_DIR=$PROD_DIR INTEL_TARGET_ARCH=$INTEL_TARGET_ARCH"
+else
+  echo "************************************************************"
+  echo "Please configure your Intel compiler if you intend to use it"
+  echo "Otherwise you need to comile with \"make COMP=gnu\""
+  echo "************************************************************"
+  
+fi
+
 if command_exists mpirun ; then
    echo "Found mpirun at: `which mpirun`"
    MPI_RUN_DIR=`dirname $(which mpirun)`
@@ -42,3 +72,4 @@ fi
 
 export MPI_CPP=$MPI_RUN_DIR/mpicxx
 echo "MPI_CPP set to $MPI_CPP"
+
